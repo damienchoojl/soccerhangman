9962cf3d904eeef672e37aee9a7a561ccffe59b6
damienchoojl <damienchoojl@gmail.com>
2023-05-15 20:35:35 +0800

    Soccer Hangman
    added id for gamePageTitle - html
    added button function for rest of the button - js

--- Project 1 - Hangman/main.js
+++ Project 1 - Hangman/main.js
@@ -2,6 +2,7 @@
 const mainPageName = prompt("Enter your name: ");
 const mainPageTitle = document.querySelector("h1");
 mainPageTitle.innerText = `Welcome to Soccer Hangman, ${mainPageName}!`;
+const gamePageTitle = document.querySelector("#gamePageTitle");
 
 /*----- state variables -----*/
 
@@ -21,10 +22,35 @@ const bodyMessage = document.querySelector("h2");
 
 /*----- event listeners -----*/
 premierLeagueButton.addEventListener("click", handlePremierLeagueButton);
+laLigaButton.addEventListener("click", handleLaLigaButton);
+seriaAButton.addEventListener("click", handleSeriaAButton);
+ligue1Button.addEventListener("click", handleLigue1Button);
 
 /*----- functions -----*/
 function handlePremierLeagueButton() {
   mainPage.classList.add("hide");
   gamePage.classList.remove("hide");
   bodyMessage.classList.add("hide");
+  gamePageTitle.innerText = `Welcome to Soccer Hangman, ${mainPageName}!`;
+}
+
+function handleLaLigaButton() {
+  mainPage.classList.add("hide");
+  gamePage.classList.remove("hide");
+  bodyMessage.classList.add("hide");
+  gamePageTitle.innerText = `Welcome to Soccer Hangman, ${mainPageName}!`;
+}
+
+function handleSeriaAButton() {
+  mainPage.classList.add("hide");
+  gamePage.classList.remove("hide");
+  bodyMessage.classList.add("hide");
+  gamePageTitle.innerText = `Welcome to Soccer Hangman, ${mainPageName}!`;
+}
+
+function handleLigue1Button() {
+  mainPage.classList.add("hide");
+  gamePage.classList.remove("hide");
+  bodyMessage.classList.add("hide");
+  gamePageTitle.innerText = `Welcome to Soccer Hangman, ${mainPageName}!`;
 }
